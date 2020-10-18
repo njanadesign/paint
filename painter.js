1b43d907ea93bd7ec4acd28b3dc62fc3ecdfc266
Noureddine JANA <nouro.jana@gmail.com>
2020-10-18 14:04:00 +0100

    Add files via upload

--- painter.js
+++ painter.js
@@ -1,75 +1,53 @@
-const canvas = document.querySelector("canvas")
-
-canvas.width = window.innerWidth
-canvas.height = window.innerHeight
+const canvas = document.querySelector('canvas');
 
 
-const context = canvas.getContext("2d")
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
-context.strokeStyle = "#ff4141"
-context.lineWidth = 6
-context.lineCap = "round"
 
-let shouldPaint = false
+const context = canvas.getContext('2d');
 
-document.addEventListener("mousedown", function (event) {
-  shouldPaint = true
-  context.moveTo(event.pageX, event.pageY)
-  context.beginPath()
-})
+context.strokeStyle = '#ff4141';
+context.lineWidth = 6;
+context.lineCap = 'round';
 
-document.addEventListener("mouseup", function (event) {
-  shouldPaint = false 
-})
+let shouldPaint = false;
 
-document.addEventListener("mousemove", function (event) {
+document.addEventListener('mousedown', function (event) {
+  shouldPaint = true;
+  context.moveTo(event.pageX, event.pageY);
+  context.beginPath();
+});
+
+document.addEventListener('mouseup', function (event) {
+  shouldPaint = false;
+});
+
+document.addEventListener('mousemove', function (event) {
   if (shouldPaint) {
-	context.lineTo(event.pageX, event.pageY)
-  context.stroke()
-  } 
-})
+    context.lineTo(event.pageX, event.pageY);
+    context.stroke();
+  }
+});
 
-document.querySelectorAll("nav a").forEach(link => {
-   link.addEventListener("click", function (event){
-  context.strokeStyle = this.style.backgroundColor 
-})                  
-          
-// _________________________________
-
-
-// Clear
-
-clear.onclick = function(){
-  context.clearRect(0, 0, canvas.width, canvas.height);
-};
-
-
-
-// _________________________________
-
-// Save
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//__________________________________________
-
-
+document.querySelectorAll('nav a').forEach((link) => {
+  link.addEventListener('click', function (event) {
+    context.strokeStyle = this.style.backgroundColor;
+  });
 
 });
 
 
 
+
+// Clear
+clear.onclick = function () {
+  context.clearRect(0, 0, canvas.width, canvas.height);
+};
+
+// Save
+document.querySelector('#save').addEventListener('click', () => {
+  canvas.toBlob(function (blob) {
+    saveAs(blob, 'canvas.png');
+  });
+});
